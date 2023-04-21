d66e38fc04d5f7f11be6628392f7deb31b40107a
kolozsvarikrisztian <laszlokrisztiankolozsvari@gmail.com>
2023-04-21 12:20:16 +0200

    first commit

---
+++ database.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE allatkert
+	CHARACTER SET utf8
+	COLLATE utf8_hungarian_ci;
---
+++ models/allat.js
@@ -0,0 +1,57 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Allat extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Allat.belongsTo(models.Kategoria, {
+        as: "Kategoria",
+        foreignKey: 'kategoriaId'
+      })
+    }
+  }
+  Allat.init({
+    nev: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    becenev: {
+        type: DataTypes.STRING(50),
+        allowNull: true
+    },
+    eletkor: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    // faj: {
+    //     type: DataTypes.STRING(50),
+    //     allowNull: false
+    // },
+    szarmazas: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    taplalek: {
+        types: DataTypes.STRING(100),
+        allowNull: true
+    },
+    kategoriaId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    kepUrl: DataTypes.STRING    
+  }, {
+    sequelize,
+    modelName: 'Allat',
+    tableName: 'aruk',
+    timestamps: false
+  });
+  return Allat;
+};
